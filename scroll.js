80886f380752e364c3976f36e7435b04f9d265bb
Zoe Cox <zoemcox16@gmail.com>
2025-07-09 15:52:41 +0100

    update

--- scroll.js
+++ scroll.js
@@ -1,12 +1,20 @@
 const headerBar = document.querySelector("#scroll-banner");
-let headerBarLeftVal = 125;
+let headerBarLeftVal = 200;
 headerBar.style.left = `${headerBarLeftVal}%`;
 
 document.addEventListener("scroll", () => {
-  if (headerBarLeftVal <= -30) {
-    return;
+  if (window.innerWidth > 2050) {
+    if (headerBarLeftVal <= -150) {
+      return;
+    } else {
+      headerBarLeftVal -= 5;
+    }
   } else {
-    headerBarLeftVal -= 5;
+    if (headerBarLeftVal <= -30) {
+      return;
+    } else {
+      headerBarLeftVal -= 5;
+    }
   }
   headerBar.style.left = `${headerBarLeftVal}%`;
 });
